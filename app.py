b268f7bbb324a4827ef196f92f585e3323b118a2
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2023-10-31 17:56:10 +0000

    game

--- app.py
+++ app.py
@@ -2,10 +2,36 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
+# write 'hello world' to the console
+import random
+l=['rock','paper','scissor']
+m=input()
 
-from flask import Flask
-app = Flask(__name__)
+while(m!='0'):
+    s=random.choice(l)
+    print('you choice: ',m)
+    print('computer choice: ',s)
+    if(s=='rock' and m=='paper'):
+        print('you won')
+    elif(s=='rock' and m=='rock'):
+        print('tie')
+    elif(s=='rock' and m=='scissor'):
+        print('you lost')
+    elif(s=='paper' and m=='rock'):
+        print('you lost')
+    elif(s=='paper' and m=='paper'):
+        print('tie')
+    elif(s=='paper' and m=='scissor'):
+        print('you won')
+    elif(s=='scissor' and m=='scissor'):
+        print('tie')
+    elif(s=='scissor' and m=='paper'):
+        print('you lost')
+    elif(s=='scissor' and m=='rock'):
+        print('you won')
+    else:
+        print('invalid')
+    m=input()
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+        
+    
